10dd1000cb4a73feb7194d42f405f4483e2c6299
DEO PRAKASH <deo.prakash@ssipmt.com>
2026-06-26 12:18:48 +0530

    Added Year and Quarter Dropdown v2

--- OKR System/okr_frontend/src/components/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SectionTitle = ({ children }) => (
-  <div className="okr-section-title">
-    <span>{children}</span>
-  </div>
-);
-
-export default SectionTitle;
